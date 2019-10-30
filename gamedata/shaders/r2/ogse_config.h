130198f8cdce8e7f61e7594d04e13d90d3de47cb
MonoLityczny <kkamil724@gmail.com>
2019-10-30 21:43:26 +0100

    - Added sunshafts (screen-space)

--- gamedata/shaders/r2/ogse_config.h
+++ gamedata/shaders/r2/ogse_config.h
@@ -26,11 +26,6 @@
 	#define SS_INTENSITY float(0.5)			// €ркость лучей, работает совместно с настройками из конфига
 	#define SS_BLEND_FACTOR float(0.8)		// фактор смешивани€ с рассе€нным светом. „ем меньше значение, тем меньше "засветка" от эффекта, но тер€етс€ "объемность"
 	#define SS_LENGTH float(1.0)			// длина лучей. „ем больше длина, тем меньше производительность
-	#define SS_DUST_SPEED float(0.4)		// скорость частиц пыли 
-	#define SS_DUST_INTENSITY float(2.0)	// €ркость пылинок
-	#define SS_DUST_DENSITY float(1.0)		// плотность частиц пыли 
-	#define SS_DUST_SIZE float(0.7)			// размер пылинок
-	#define SS_DUST
 
 /////////////////////////////////////////////////////////////
 // volumetric light
